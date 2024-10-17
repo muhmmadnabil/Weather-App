bffbfc43b8384bd326d0277c83310dbb0cc85dcf
Muhmmad Nabil <muhmmad.nabil.albadry@gmail.com>
2024-10-17 04:05:12 +0300

    Initial commit

---
+++ app/src/main/java/com/muhmmad/weatherapp/navigation/WeatherNavigation.kt
@@ -0,0 +1,18 @@
+package com.muhmmad.weatherapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.muhmmad.weatherapp.screens.SplashScreen
+
+@Composable
+fun WeatherNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = WeatherScreens.SplashScreen.name) {
+        composable(WeatherScreens.SplashScreen.name) {
+            SplashScreen(navController = navController)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/muhmmad/weatherapp/screens/SplashScreen.kt
@@ -0,0 +1,10 @@
+package com.muhmmad.weatherapp.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    Text("Splash Screen")
+}
